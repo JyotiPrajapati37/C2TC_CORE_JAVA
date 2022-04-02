fbc4b0545204bd20c1857b05521ade6fd147c588
admin <admin@admin-PC>
2022-04-02 13:11:33 +0530

    Exception

---
+++ src/Demo/WithException.java
@@ -0,0 +1,17 @@
+package Demo;
+
+public class WithException {
+
+	public static void main(String[] args) {
+		System.out.println("Hii");
+		try {
+			System.out.println(10/0);
+		}
+		catch(Exception e)
+		{
+			System.out.println(10/2);
+			System.out.println(e);
+	}
+		System.out.println("Hello");
+}
+}
